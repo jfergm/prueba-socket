66d98230a123821b484b3963067325dc0e90830d
José Fernando <jose.gomezm12@gmail.com>
2020-12-01 00:56:13 -0600

    exam

---
+++ src/app.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import socketio from 'socket.io';
+
+import dotEnv from 'dotenv';
+
+import {countShipments} from './getShipments';
+
+
+dotEnv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+let counter = 0;
+
+
+const httpServer = app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`)
+})
+
+const io = socketio(httpServer);
+
+(async () => {
+  counter = await countShipments();
+  io.emit('counter', counter)
+})();
+
+io.on('connection', (socket) => {
+  io.emit('counter', counter)
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/webhook/shipments', ({res}) => {
+  io.emit('counter', ++counter);
+
+  res.status(200).send();
+})
---
+++ src/getShipments.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+const date = new Date();
+const month = date.getMonth() + 1;
+const year = date.getFullYear();
+
+const getShipments = async () => {
+
+  try {
+    const data = await axios({
+      method: 'get',
+      url: `http://${process.env.QUERY_ENDPOINT}/guide/${month - 1}/${year}`,
+      headers: {
+        'Authorization': `Bearer ${process.env.API_KEY}`
+      }
+    })
+    return data.data.data
+  } catch(e) {
+    console.log(e)
+  }
+
+  return []
+}
+
+const countShipments = async () => {
+  const shipments = await getShipments();
+
+  return await shipments.length;
+}
+
+export default getShipments;
+
+export { countShipments };
